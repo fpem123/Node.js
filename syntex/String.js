cc9a487e3a6f190784edd42adac16eebe15c4b99
fpem123 <ghtjq3909@naver.com>
2020-11-01 00:40:28 +0900

    20/11/01 committed

---
+++ syntex/String.js
@@ -0,0 +1,2 @@
+console.log('1'+'1');
+console.log('안녕하세요 감사해요 잘있어요 다시 만나요.'.length);
---
+++ syntex/Variable.js
@@ -0,0 +1,8 @@
+let a = 1;
+const PI = 3.14;
+
+console.log(a);
+a = 2;
+console.log(a);
+console.log(PI);
+// PI = 3; comment
